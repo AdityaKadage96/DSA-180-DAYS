1d5d1a266a558e3cf59a120ef7b2f18e8eca6d35
Aditya Kadage <adityakadage2021@gmail.com>
2025-11-18 19:59:15 +0530

    solved min/max, odd/even basic problem

--- DSAPractice/Array/userInputArray3.java
+++ DSAPractice/Array/userInputArray3.java
@@ -20,44 +20,119 @@ public class userInputArray3 {
     //   }
     //   System.out.println("Sum of array is:"+ sum);
 
-      //-------------------------------------------------------------------
+      //----------------------------------------------------------------------------------------------------------------------
 
     //2.Find key element in array
-    System.out.println("Enter size of array:");
-    Scanner scI=new Scanner(System.in);
-    int size=scI.nextInt();
-    int arr[]=new int[size];
+    // System.out.println("Enter size of array:");
+    // Scanner scI=new Scanner(System.in);
+    // int size=scI.nextInt();
+    // int arr[]=new int[size];
 
-    System.out.println("Enter array elements:");
-    for(int i=0;i<arr.length;i++){
-        arr[i]=scI.nextInt();
-    }
+    // System.out.println("Enter array elements:");
+    // for(int i=0;i<arr.length;i++){
+    //     arr[i]=scI.nextInt();
+    // }
     
-    System.out.println("Array elments which we enter:");
-    for(int i=0;i<arr.length;i++){
-        System.out.print(arr[i] +" ");
-    }
-    System.out.println();
+    // System.out.println("Array elments which we enter:");
+    // for(int i=0;i<arr.length;i++){
+    //     System.out.print(arr[i] +" ");
+    // }
+    // System.out.println();
     
-    System.out.println("Enter num which is you want to find in array:");
-    int num=scI.nextInt();
+    // System.out.println("Enter num which is you want to find in array:");
+    // int num=scI.nextInt();
     
-    boolean found=false;
-    for(int i=0;i<arr.length;i++){
+    // boolean found=false;
+    // for(int i=0;i<arr.length;i++){
 
-        if(num==arr[i]){
-            System.out.println("KEY FOUND WHICH ENTER YOU");
-            found=true;
-            break;
-        }
-    }
+    //     if(num==arr[i]){
+    //         System.out.println("KEY FOUND WHICH ENTER YOU");
+    //         found=true;
+    //         break;
+    //     }
+    // }
 
-    if(found==true){
-        System.out.println("key is found");
-    }else{
-        System.out.println("key is Not found");
-    }
+    // if(found==true){
+    //     System.out.println("key is found");
+    // }else{
+    //     System.out.println("key is Not found");
+    // }
+    //--------------------------------------------------------------------------------------------------------------------
 
+    //3.Find max element in an array
+
+    // int arr[]={10,5,29,45,6};
+
+    // int max=Integer.MIN_VALUE;
+    // int size=arr.length;
+    // for(int i=0;i<size;i++){
+    //   if(arr[i]>max){
+    //     max=arr[i];
+    //   }
+
+    // }
+    // System.out.println("max element:" + max);
+
+    //By taking user input from user
+    
+    //  System.out.println("Enter size of array:");
+    //  Scanner sc=new Scanner(System.in);
+    //  int size=sc.nextInt();
+    //  int arr[]=new int[size];
+     
+    //  System.out.println("Enter array element:");
+    //  for(int i=0;i<arr.length;i++){
+    //      arr[i]=sc.nextInt();
+    //  }
+
+    //  int max=Integer.MIN_VALUE;
+
+    //  for(int i=0;i<arr.length;i++){
+    //     if(arr[i]>max){
+    //         max=arr[i];
+    //     }
+    //  }
+
+    //  System.out.println("Max element in an array:" + max);
+//--------------------------------------------------------------------------------------------------------------------------------------
+    //4.Find Min element in an array
+    
+    // int arr[]={15,5,25,6,7,88};
+    // int size=arr.length;
+
+    // int min=Integer.MAX_VALUE;
+
+    // for(int i=0;i<size;i++){
+    //     if(arr[i]<min){
+    //         min=arr[i];
+    //     }
+    // }
+
+    //System.out.println("Minimum Element in an array:"+min);
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+    //5.Even and Odd Number count in an array
+
+    // int arr[]={10,3,16,15,8,25,12};
+    // int size=arr.length;
+    // int even=0;
+    // int odd=0;
+    // for(int i=0;i<size;i++){
+    //     if(arr[i]%2==0){
+    //         even++;
+    //     }
+    //     if(arr[i]%2==1){
+    //         odd++;
+    //     }
+    // }
+
+    // System.out.println("Even Number count:"+even);
+    // System.out.println("Odd number count:"+odd);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+    
 
 
     }
