1d2734837fea8f04af8e618008484443658eba1a
Aditya Kadage <adityakadage2021@gmail.com>
2025-12-17 22:56:57 +0530

    prefix sum simple problem

--- PrefixSum/arrayDay30.java
+++ PrefixSum/arrayDay30.java
@@ -1,5 +1,7 @@
 package PrefixSum;
 
+import java.util.Arrays;
+
 public class arrayDay30 {
     public static int sumOddLengthSubarrays(int[] arr) {
         int n = arr.length;
@@ -22,11 +24,28 @@ public class arrayDay30 {
 
         return totalSum;
     }
+
+    public static int[] runningSum(int[] nums) {
+        int n=nums.length;
+
+        for(int i=1;i<n;i++){
+            nums[i]=nums[i]+nums[i-1];
+        }
+        return nums;
+    }
     public static void main(String[]args){
         //1.Sum of all odd length subarray
         int arr[]={1,4,2,5,3};
         sumOddLengthSubarrays(arr);
         System.out.println(sumOddLengthSubarrays(arr));
+
+
+        //Day31.Running sum of 1d array
+
+        int arr2[]={1,2,3,4};
+        runningSum(arr2);
+        System.out.println(Arrays.toString(runningSum(arr2)));
+        
 
 
         
