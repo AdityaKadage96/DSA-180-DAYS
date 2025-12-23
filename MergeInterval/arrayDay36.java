b7efe39b0790aeb3e9f0a3e60b854968f03f3db3
Aditya Kadage <adityakadage2021@gmail.com>
2025-12-23 21:16:47 +0530

    non overlapping intervals

--- MergeInterval/arrayDay36.java
+++ MergeInterval/arrayDay36.java
@@ -1,6 +1,7 @@
 package MergeInterval;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class arrayDay36 {
@@ -29,6 +30,26 @@ public class arrayDay36 {
 
         return res.toArray(new int[res.size()][]);
     }
+
+    public static int nonOverlapIntervals(int[][] intervals) {
+        
+        Arrays.sort(intervals,(a,b) -> a[1]-b[1]);
+
+        int count=0;
+        int end=intervals[0][1];
+
+        for(int i=1;i<intervals.length;i++){
+            
+            if(intervals[i][0] < end){
+                count++;
+            }else{
+                end=intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+    
     public static void main(String[]args){
      //1.Interval List intersection
      int[][] firstList  = {{0,2},{5,10},{13,23},{24,25}};
@@ -39,5 +60,17 @@ public class arrayDay36 {
     for(int[] a : ans){
         System.out.println(a[0] + " " + a[1]);
     }
+
+    //Day 37 - Non overlapping intervals
+
+    int [][]arr={{1,2},{2,3},{3,4},{1,3}};
+    nonOverlapIntervals(arr);
+    System.out.println(nonOverlapIntervals(arr));
+    
+
+
+
+
+
     }
 }
