2f19ae55eefde139219598101408233899f5d7b2
Aditya Kadage <adityakadage2021@gmail.com>
2025-12-30 22:17:42 +0530

    merge interval based problem

--- MergeInterval/arrayDay43.java
+++ MergeInterval/arrayDay43.java
@@ -21,11 +21,35 @@ public class arrayDay43 {
 
          return res;
     }
+
+    public static int minimumChairs(String s) {
+        int curr=0;
+        int max=0;
+
+
+        for(char c : s.toCharArray()){
+            
+            if(c == 'E'){
+                curr++;
+            }else{
+                curr--;
+            }
+
+            max=Math.max(max,curr);
+        }
+
+        return max;
+    }
     public static void main(String[]args){
         //1.Plus one
 
         int arr[]={1,2,3};
         plusOne(arr);
         System.out.println(Arrays.toString(plusOne(arr)));
+
+        //2.Minimum numbers of chairs in a Waiting
+        String s="EEEEEEE";
+        minimumChairs(s);
+        System.out.println(minimumChairs(s));
     }
 }
