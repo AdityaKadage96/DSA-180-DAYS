8069938360e757dd1a6d1b88ae74ed912b8c5bb7
Aditya Kadage <adityakadage2021@gmail.com>
2026-01-06 21:16:32 +0530

    remove outermost parantheses stack based problem

--- Stack/stackDay50.java
+++ Stack/stackDay50.java
@@ -16,10 +16,42 @@ public class stackDay50 {
 
         return x;
     }
+
+    public static String removeOuterParentheses(String s) {
+        StringBuilder res= new StringBuilder();
+
+        int op=0;
+
+        for(char c: s.toCharArray()){
+
+            if(c=='('){
+                if(op > 0){
+                    res.append(c);
+                }
+
+                op++;
+            }else{
+
+                 op--;
+                if(op > 0){
+                   
+                    res.append(c);
+                }
+            }
+        }
+
+        return res.toString();
+    }
     public static void main(String[]args){
         //1. Final value of variable after peroforming operation
         String s[]={"--X","X++","X++"};
         finalValueAfterOperations(s);
         System.out.println(finalValueAfterOperations(s));
+
+        //2.Remove Outermost parantheses
+
+        String s2 = "(()())(())";
+        removeOuterParentheses(s2);
+        System.out.println(removeOuterParentheses(s2));
     }
 }
