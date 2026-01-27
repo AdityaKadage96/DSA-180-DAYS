78a838070900b75d2cfa3ceaf146a92d9741074a
Aditya Kadage <adityakadage2021@gmail.com>
2026-01-27 22:52:02 +0530

    find first and last position of element in sorted order

--- BinarySearch/binarySearchDay71.java
+++ BinarySearch/binarySearchDay71.java
@@ -20,6 +20,63 @@ public class binarySearchDay71 {
 
         return l;
     }
+
+    public static int[] searchRange(int[] nums, int target) {
+        
+
+        if(nums==null || nums.length==0){
+            return new int []{-1,-1};
+        }
+        int first=findFirst(nums,target);
+        int second=findSecond(nums,target);
+
+        return new int[]{first,second};
+    }
+
+    
+
+    public static int findFirst(int nums[],int target){
+        int l=0;
+        int h=nums.length-1;
+
+        int ans=-1;
+        while (l<=h){
+            int mid=l+(h-l)/2;
+
+            if(nums[mid]==target){
+                ans=mid;
+                h=mid-1;
+            }else if(nums[mid]<target){
+                l=mid+1;
+            }else {
+                h=mid-1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int findSecond(int nums[], int target){
+        int l=0;
+        int h=nums.length-1;
+        int ans=-1;
+
+        while(l<=h){
+            int mid=l+(h-l)/2;
+
+            if(nums[mid]==target){
+                ans=mid;
+                l=mid+1;
+
+            }else if(nums[mid]<target){
+                l=mid+1;
+            }else {
+                h=mid-1;
+            }
+        }
+
+        return ans;
+    }
     public static void main(String []args){
        //1.Search Insert Position 
 
@@ -27,5 +84,13 @@ public class binarySearchDay71 {
        int tar=5;
        searchInsert(arr, tar);
        System.out.println(searchInsert(arr, tar));
+
+
+       //2.find first and last position of element in sorted order
+
+       int arr2[]={5,7,7,8,8,10};
+       int target=8;
+       searchRange(arr2, target);
+       System.out.println(searchRange(arr2, target));
     }
 }
