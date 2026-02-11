7597655c7b8be96b6e6602166e6814648acdcb8b
Aditya Kadage <adityakadage2021@gmail.com>
2026-02-11 19:45:44 +0530

    queue based problem pass the pillow

--- Queue/queueDay86.java
+++ Queue/queueDay86.java
@@ -15,6 +15,18 @@ public class queueDay86{
 
         return ti;
     }
+
+    public static int passThePillow(int n, int time){
+        int cycle=2*(n-1);
+        int t=time % cycle;
+
+        if(t<n){
+            return t+1;
+    
+        }else{
+            return n-(t-(n-1));
+        }
+    }
     public static void main(String []args){
       //Day 86-Time needed to buy tickets
 
@@ -22,5 +34,13 @@ public class queueDay86{
       int k=2;
       timeRequiredToBuy(ticket, k);
       System.out.println(timeRequiredToBuy(ticket, k));
+
+
+      //Day 87- Pass the pillow
+
+      int time=5;
+      int n=4;
+      passThePillow(n, time);
+      System.out.println(passThePillow(n, time));
     }
 }
